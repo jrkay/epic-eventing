ff4ee42c37809bfd42fb4351143a0824fae7a06d
jrkay <jrkohlmeyer@gmail.com>
2020-03-26 17:33:59 -0700

    Yarn build

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,17 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
-      }
+      },
+      { test: /\.css$/, use: 'css-loader' },
+      { test: /\.ts$/, use: 'ts-loader' },
+      {
+      	test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+   		loader: 'url-loader?limit=100000' }
     ]
-  }
+  },
+ performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+}
 };
