f3ab4c157b444d5ee45ee43017ad31842924dd13
jrkay <jrkohlmeyer@gmail.com>
2020-03-26 19:13:47 -0700

    Set up

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';import './App.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
